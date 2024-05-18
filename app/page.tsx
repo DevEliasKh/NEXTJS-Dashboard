9108faa46cb7dd9fb1462c0330fb1aa6da47c6b6
develiaskh <develiaskh@gmail.com>
2024-05-18 11:21:29 -0700

    add lesson 16

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
-import Link from 'next/link';
 import Image from 'next/image';
+import Link from 'next/link';
+import { Metadata } from 'next';
+import { lusitana } from '@/app/ui/fonts';
 
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
